fc3350ef03895fe21855700fe2a62df45057e5b3
jevgenijs.cernuha <jc_mogo@yahoo.com>
2020-02-09 14:50:02 +0200

    commit

--- src/test/java/DelfiFirstTest.java
+++ src/test/java/DelfiFirstTest.java
@@ -3,13 +3,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelfiFirstTest {
-}
 
-public class  DelfiFirstTest {
     @Test
-    public void delfiFirstTitleTest (){
+    public void delfiFirstTitleTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/jevgenijs.cernuha/IdeaProjects/chromedriver");
         WebDriver driver = new ChromeDriver();
-
-
+        driver.manage().window().maximize();
+        driver.get("http://delfi.lv");
     }
 }
