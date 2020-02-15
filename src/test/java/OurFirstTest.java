143acb623d4aa651e6e834e2e706894a907b5975
jevgenijs.cernuha <jc_mogo@yahoo.com>
2020-02-15 20:14:02 +0200

    commit

--- src/test/java/OurFirstTest.java
+++ src/test/java/OurFirstTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 public class OurFirstTest {
     @Test
     public void firstTest () {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 
 }
